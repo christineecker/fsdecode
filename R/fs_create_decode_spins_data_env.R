99a5c133318a8620807adbb948c4de93f1177d07
Christine Ecker <67059519+christineecker@users.noreply.github.com>
2024-07-19 10:50:30 +0200

    implemented two functions
    fs_create_decode_spins_data_env and fs_decode_spins

--- R/fs_create_decode_spins_data_env.R
+++ R/fs_create_decode_spins_data_env.R
@@ -2,14 +2,14 @@
 #'
 #' Creates data enviroment for `fs_decode_spins`.
 #'
-#' @details Note.
+#' @details Note. vertices in medial wall label are set to zero.
 #'
 #' @param hemi "lh", "rh", or "both". Default is "both"
 #' @param genes Gene list of genes to decode as symbols. Default is all abagen genes.
 #'
 #' @export
 #'
-#' @return Data environment with `mRNA`, `permuted.pcs`, and `gene.allocation`.
+#' @return Data environment with `mRNA` expression data
 #'
 #' @examples
 #' data.env <- fs_create_decode_spins_data_env("lh")
@@ -21,7 +21,6 @@ fs_create_decode_spins_data_env <- function(hemi = "both",
 
   ########################### Load data into workspace ###########################
 
-  writeLines("\nLoading mRNA expression maps ... ")
   mRNA <- load_mRNA_expression_maps()$lh.rh.mRNA.fsavg6.fwhm5
 
   ############################## Create environment ##############################
@@ -36,12 +35,15 @@ fs_create_decode_spins_data_env <- function(hemi = "both",
   if (hemi == "lh") {
     writeLines("\nCreating data environment for left hemisphere ... ")
     data.env$mRNA <- mRNA$lh
+    data.env$mRNA[,fsavg6$medial.wall.verts$lh] <- 0
   } else if  (hemi == "rh") {
     writeLines("\nCreating data environment for right hemisphere ... ")
     data.env$mRNA <- mRNA$rh
+    data.env$mRNA[,fsavg6$medial.wall.verts$rh] <- 0
   } else if  (hemi == "both") {
-    writeLines("\nCreating data environment for both hemispheres ... ")
+    writeLines("\nConcatenating data across hemispheres ... ")
     data.env$mRNA <- cbind(mRNA$lh, mRNA$rh)
+    data.env$mRNA[,fsavg6$medial.wall.verts$both] <- 0
   } else {
     writeLines("\nPlease specify either 'lh', 'rh', or 'both'")
   }
@@ -55,4 +57,6 @@ fs_create_decode_spins_data_env <- function(hemi = "both",
 #                                                                              #
 ################################################################################
 
-# data.env <- fs_create_decode_spins_data_env("both")
+# data.env <- fs_create_decode_spins_data_env(hemi = "both",
+#                                             genes = abagen.genes$symbol[1:10])
+# data.env$mRNA[1:10, 1:10]
--- R/fs_decode_spins.R
+++ R/fs_decode_spins.R
@@ -1,42 +1,63 @@
 
-
-fs_decode_spins <- function()
+#' fs_decode_spins
+#'
+#' Performs gene expression decoding of FreeSurfer overlay using Alexander-Bloch spin model.
+#' Requires that medial wall labels are set to O rather than NA.
+#'
+#' @param fs.overlay FreeSurfer fsaverage6 overlay for both hemispheres
+#' @param data.env Data environment with mRNA data
+#' @param n.spins Number of spins
+#' @param genes Gene list in symbols
+#'
+#' @export
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' # ADD_EXAMPLES_HERE
+fs_decode_spins <- function(fs.overlay,
+                            data.env = NULL,
+                            n.spins = 1000L,
+                            genes = abagen.genes$symbol)
 {
-  # source("R/row_correlation_eigen.R")
-  # pet <- c(pet.GABAABZ$lh, pet.GABAABZ$rh)
-  # pet[fsavg6$medial.wall.verts$both] <- 0
-  #
-  # n.spins <- 1000L
-  # pet.spins <- fsnulls::fs_create_spins_bloch(pet, n.spins)
-  # fs_plot(pet.spins[1,])
-  #
-  # n.verts <- 81924
-  # n.genes <- 1600
-  # genes <- matrix(runif(n.genes*n.verts, 0, 1), n.genes, n.verts)
 
+  ##| create data environment --------------------------------------------------------
+  ##|
+
+  fs.overlay[fsavg6$medial.wall.verts$both] <- 0
+
+  data.env <- NULL
+  if(is.null(data.env)){
+    data.env <- new.env()
+    data.env <- fs_create_decode_spins_data_env("both", genes)
+  }
+
+  ##| compute spins --------------------------------------------------------
+  ##|
+
+  fs.overlay.spins <- fsnulls::fs_create_spins_bloch(fs.overlay, n.spins)
+  fs_plot(fs.overlay.spins[2,])
 
   ##| compute observed correlations  --------------------------------------------------------
   ##|
 
+  source("R/row_corr_eigen.R")
   writeLines("\nComputing observed correlations between overlay and all genes ...")
   r.obs <- vector()
-  r.obs <- as.vector(row_cor_eigen(genes, t(pet))) #| n.genes
+  r.obs <- as.vector(row_cor_eigen(data.env$mRNA, t(fs.overlay))) #| n.genes
 
   ##| compute null correlations --------------------------------------------------------
   ##|
 
-  source("R/row_correlation_eigen.R")
   writeLines("\nComputing null correlations between overlay spins and genes ...")
   r.null <- matrix()
-  r.null <- row_cor_eigen(genes, pet.spins) #| n.genes x n.spins
-
+  r.null <- row_cor_eigen(data.env$mRNA, fs.overlay.spins) #| n.genes x n.spins
 
   ##| compute permutation p value --------------------------------------------------------
   ##|
 
   writeLines("\nComputing permutation p-values ...")
   p.perm <- vector()
-  for (i in 1:n.genes) {
+  for (i in 1:nrow(data.env$mRNA)) {
     ## compute two-tailed permutation p value
     p.perm[i] = (sum(abs(r.null[i,]) >= abs(r.obs[i])) + 1) / (1 + n.spins)
   }
@@ -49,6 +70,12 @@ fs_decode_spins <- function()
   ##| create data.table with results --------------------------------------------------------
   ##|
 
+  gene.index <- which(abagen.genes$symbol %in% genes)
+  genes.decoded <- data.table::data.table("gene.symbol" = genes,
+                                          "gene.entrez" = abagen.genes$entrez[gene.index],
+                                          "r" = r.obs,
+                                          "p.perm" = p.perm,
+                                          "p.fdr.adj" = p.fdr.adj)
 
 
   return(genes.decoded)
@@ -59,3 +86,16 @@ fs_decode_spins <- function()
 #                                   Testing                                    #
 #                                                                              #
 ################################################################################
+
+# fs.overlay <- c(pet.GABAABZ$lh, pet.GABAABZ$rh)
+# data.env <- NULL
+# n.spins <- 100L
+# genes <- abagen.genes$symbol[1:1000]
+#
+# genes.decoded <- fs_decode_spins(fs.overlay,
+#                                  data.env = data.env,
+#                                  n.spins = n.spins,
+#                                  genes = genes)
+# str(genes.decoded)
+
+
