c227343c6c4b9dbaba69f55cf4357c1013920d27
Christine Ecker <67059519+christineecker@users.noreply.github.com>
2024-07-30 11:39:21 +0200

    minor modifications

--- R/fs_create_decode_sa_null_data_env.R
+++ R/fs_create_decode_sa_null_data_env.R
@@ -35,15 +35,15 @@ fs_create_decode_sa_null_data_env <- function(hemi="both", genes=abagen.genes$sy
 
   data.env <- new.env()
   if (hemi == "lh") {
-    writeLines("\nCreating data environment for left hemisphere ... ")
+    #writeLines("\nCreating data environment for left hemisphere ... ")
     data.env$mRNA <- mRNA$lh
     data.env$permuted.pcs <- permuted.pcs$lh
   } else if  (hemi == "rh") {
-    writeLines("\nCreating data environment for right hemisphere ... ")
+    #writeLines("\nCreating data environment for right hemisphere ... ")
     data.env$mRNA <- mRNA$rh
     data.env$permuted.pcs <- permuted.pcs$rh
   } else if  (hemi == "both") {
-    writeLines("\nConcatenating across hemispheres ... ")
+    #writeLines("\nConcatenating across hemispheres ... ")
     data.env$mRNA <- cbind(mRNA$lh, mRNA$rh)
     for (i in 1:9) {
       data.env$permuted.pcs[[i]] <- cbind(permuted.pcs$lh[[i]], permuted.pcs$rh[[i]])
