78392e2516157401e9fb6181558845310166383e
Christine Ecker <67059519+christineecker@users.noreply.github.com>
2024-08-01 10:23:46 +0200

    minor modifications

--- R/fs_decode_with_nulls.R
+++ R/fs_decode_with_nulls.R
@@ -1,12 +1,11 @@
 
-
 #' fs_decode_with_nulls
 #'
-#' Performs gene expression decoding of FreeSurfer fsaverage6 overlay based on precomputed surrogate maps
-#' Note. requires n.perm x n.verts surrogate matrix
+#' Performs gene expression decoding of FreeSurfer fsaverage6 overlay based on precomputed null maps
+#' Note. requires null matrix with dimensions n.nulls x n.verts
 #'
 #' @param fs.overlay FreeSurfer fsaverage6 overlay for both hemispheres
-#' @param fs.surrogates n.perm x n.verts matrix of permuted maps
+#' @param fs.surrogates Matrix of null models with dimensions `n.nulls` x `n.verts`
 #' @param data.env Data environment with mRNA data
 #' @param genes Gene list in symbols
 #'
@@ -16,7 +15,7 @@
 #' @examples
 #' # ADD_EXAMPLES_HERE
 fs_decode_with_nulls <- function(fs.overlay,
-                                 fs.surrogates = NULL,
+                                 fs.nulls = NULL,
                                  data.env = NULL,
                                  genes = abagen.genes$symbol)
 {
@@ -38,7 +37,7 @@ fs_decode_with_nulls <- function(fs.overlay,
     data.env <- fs_create_decode_spins_data_env("both", genes) #| $mRNA: n.genes x n.verts
   }
 
-  if(is.null(fs.surrogates)){
+  if(is.null(fs.nulls)){
     writeLines("\nRequires surrogate maps with dimensions n.perm x n.verts ...")
     break
   }
@@ -56,19 +55,19 @@ fs_decode_with_nulls <- function(fs.overlay,
 
   writeLines("\nComputing null correlations between overlay spins and genes ...")
   r.null <- matrix()
-  r.null <- row_cor_eigen(data.env$mRNA, fs.surrogates) #| returns n.genes x n.spins
+  r.null <- row_cor_eigen(data.env$mRNA, fs.nulls) #| returns n.genes x n.spins
 
   ##| compute permutation p value --------------------------------------------------------
   ##|
 
-  n.surrogates <- nrow(fs.surrogates)
+  n.nulls <- nrow(fs.nulls)
   n.genes <- nrow(data.env$mRNA)
 
   writeLines("\nComputing permutation p-values ...")
   p.perm <- vector()
   for (i in 1:n.genes) {
     ## compute two-tailed permutation p value
-    p.perm[i] = (sum(abs(r.null[i,]) >= abs(r.obs[i])) + 1) / (1 + n.surrogates)
+    p.perm[i] = (sum(abs(r.null[i,]) >= abs(r.obs[i])) + 1) / (1 + n.nulls)
   }
 
   ##| compute maxT corrected p values --------------------------------------------------------
@@ -79,7 +78,7 @@ fs_decode_with_nulls <- function(fs.overlay,
   # get maximum absolute correlation across genes within permutations
   maxR <- apply(abs(r.null), 2, function(x) {max(x, na.rm = T)})
   for (i in 1:n.genes) {
-    p.adj[i] = (sum(maxR >= abs(r.obs[i])) + 1) / (1 + n.surrogates)
+    p.adj[i] = (sum(maxR >= abs(r.obs[i])) + 1) / (1 + n.nulls)
   }
 
   ##| compute fdr adjusted p-values --------------------------------------------------------
@@ -110,11 +109,11 @@ fs_decode_with_nulls <- function(fs.overlay,
 
 # fs.overlay <- c(pet.5HT$`5HT1A`$lh, pet.5HT$`5HT1A`$rh)
 #
-# spins <- fsnulls::fs_create_spins_bloch(fs.overlay, 100L)
+# nulls <- fsnulls::fs_create_spins_bloch(fs.overlay, 100L)
 #
 # genes.decoded <- fs_decode_with_nulls (
 #   fs.overlay,
-#   fs.surrogates = spins,
+#   fs.nulls = nulls,
 #   data.env = NULL,
 #   genes = abagen.genes$symbol
 # )
